d587aba28363e52fa78485d5039fc4c2b8cbfd23
Mark Rattle <mark.rattle@gmail.com>
2023-04-16 18:38:20 -0400

    users can log in, needs async treatment

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+const user = require('../models/user');
 
 module.exports = (app) => {
   // SIGN UP FORM
@@ -20,5 +21,69 @@ module.exports = (app) => {
       console.log(err.message);
       return res.status(400).send({ err });
     }
+  });
+  // LOGOUT
+  app.get('/logout', (req, res) => {
+    res.clearCookie('nToken');
+    return res.redirect('/');
+  });
+  // LOGIN FORM
+  app.get('/login', (req, res) => res.render('login'));
+  // // LOGIN POST ACTION
+  // app.post('/login', async (req, res) => {
+  //   try {
+  //     const { username, password } = req.body;
+  //     foundUser = await User.findOne({ username }, 'username password')
+  //   } catch(err) {
+  //     console.log(err);
+  //     return res.status(401).send({ message: 'Wrong Username or Password' });
+  //   };
+  //   try {
+  //     user.comparePassword(password, (err, isMatch) => {
+  //       if (!isMatch) {
+  //         // Password does not match
+  //         return res.status(401).send({ message: 'Wrong Username or password' });
+  //       }
+  //       // Create a token
+  //       const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+  //         expiresIn: '60 days',
+  //       });
+  //       // Set a cookie and redirect to root
+  //       res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+  //       return res.redirect('/');
+  //     });
+  //   } catch(err) {
+  //     console.log(err);
+  //     // return res.status(401).send({ message: 'Wrong Username or Password' });
+  //   };
+  // });
+  // LOGIN
+  app.post('/login', (req, res) => {
+    const { username, password } = req.body;
+    // Find this user name
+    User.findOne({ username }, 'username password')
+      .then((user) => {
+        if (!user) {
+          // User not found
+          return res.status(401).send({ message: 'Wrong Username or Password' });
+        }
+        // Check the password
+        user.comparePassword(password, (err, isMatch) => {
+          if (!isMatch) {
+            // Password does not match
+            return res.status(401).send({ message: 'Wrong Username or password' });
+          }
+          // Create a token
+          const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+            expiresIn: '60 days',
+          });
+          // Set a cookie and redirect to root
+          res.cookie('nToken', token, { maxAge: 900000, httpOnly: true });
+          return res.redirect('/');
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   });
 };
